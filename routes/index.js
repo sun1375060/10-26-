f09c04320d293496f7012db80c2240d466637682
sunjiaxin <1375060730@qq.com>
2017-10-27 09:29:21 +0800

    地方规定

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : '123456',
-  port:3306
+  password : '123456'
 });
 /* GET home page. */
 router.post('/', function(req, res, next) {
