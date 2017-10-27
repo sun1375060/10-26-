d5a3f9542f7e83fdeff1414fd54525c919e3e66c
houlucheng <1986222509@qq.com>
2017-10-27 09:23:18 +0800

    asdasd

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : '123456'
+  password : '123456',
+  port:3306
 });
 /* GET home page. */
 router.post('/', function(req, res, next) {
