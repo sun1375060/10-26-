e59ff547b54aa72dbbbf43e04bbb7cb06653b362
sunjiaxin <1375060730@qq.com>
2017-10-27 08:59:43 +0800

    10-26  作业

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '123456'
+});
+/* GET home page. */
+router.post('/', function(req, res, next) {
+  connection.connect();
+  connection.query('SELECT * FROM aaa.BBB', function(err, rows, fields) {
+  	console.log(rows)
+    res.header('Access-Control-Allow-Origin',"*")
+    res.send(rows)
+  });
+  connection.end();
+});
+
+module.exports = router;
